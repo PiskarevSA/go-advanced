03cba41ea0f36c22e802ca3ec0805e1489fe33d8
Semen Piskarev <piskarevsa@mail.ru>
2025-02-25 21:38:19 +0300

    server: listen and serve localhost:8080

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import "net/http"
+
+// run server successfully or return error to panic in the main()
+func run() error {
+	mux := http.NewServeMux()
+	err := http.ListenAndServe("localhost:8080", mux)
+	return err
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		panic(err)
+	}
+}
