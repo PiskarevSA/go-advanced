89f2b64c85145c9522c754fd0c02224251a5a251
Semen Piskarev <piskarevsa@mail.ru>
2025-03-01 16:57:43 +0300

    server: move handler to separate package

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -2,9 +2,8 @@ package server
 
 import (
 	"net/http"
-	"strconv"
-	"strings"
 
+	"github.com/PiskarevSA/go-advanced/internal/handlers"
 	"github.com/PiskarevSA/go-advanced/internal/storage"
 )
 
@@ -21,60 +20,7 @@ func NewServer() *Server {
 // run server successfully or return error to panic in the main()
 func (s *Server) Run() error {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/`, s.Update)
+	mux.HandleFunc(`/update/`, handlers.Update(s.storage))
 	err := http.ListenAndServe("localhost:8080", mux)
 	return err
-}
-
-func (s *Server) Update(res http.ResponseWriter, req *http.Request) {
-	// method be POST
-	if req.Method != http.MethodPost {
-		http.NotFound(res, req)
-		return
-	}
-	// header should contains "Content-Type: text/plain"
-	if req.Header.Get("Content-Type") != "text/plain" {
-		http.Error(res, "supported Content-Type: text/plain",
-			http.StatusBadRequest)
-		return
-	}
-	// incorrect metric type should return http.StatusBadRequest
-	tail := strings.TrimPrefix(req.URL.Path, "/update/")
-	metricType, tail, _ := strings.Cut(tail, "/")
-	metricName, metricValue, _ := strings.Cut(tail, "/")
-
-	switch metricType {
-	case "gauge":
-		if len(metricName) == 0 {
-			http.NotFound(res, req)
-			return
-		}
-		f64, err := strconv.ParseFloat(metricValue, 64)
-		if err != nil {
-			http.Error(res, "incorrect metric value",
-				http.StatusBadRequest)
-			return
-		}
-		s.storage.SetGauge(metricName, f64)
-	case "counter":
-		if len(metricName) == 0 {
-			http.NotFound(res, req)
-			return
-		}
-		i64, err := strconv.ParseInt(metricValue, 10, 64)
-		if err != nil {
-			http.Error(res, "incorrect metric value",
-				http.StatusBadRequest)
-			return
-		}
-		s.storage.IncreaseCounter(metricName, i64)
-	case "":
-		http.Error(res, "empty metric type",
-			http.StatusBadRequest)
-		return
-	default:
-		http.Error(res, "unexpected metric type",
-			http.StatusBadRequest)
-		return
-	}
 }
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type repositories interface {
+	SetGauge(key string, value float64)
+	IncreaseCounter(key string, addition int64)
+}
+
+func Update(repo repositories) func(res http.ResponseWriter, req *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		// method be POST
+		if req.Method != http.MethodPost {
+			http.NotFound(res, req)
+			return
+		}
+		// header should contains "Content-Type: text/plain"
+		if req.Header.Get("Content-Type") != "text/plain" {
+			http.Error(res, "supported Content-Type: text/plain",
+				http.StatusBadRequest)
+			return
+		}
+		// incorrect metric type should return http.StatusBadRequest
+		tail := strings.TrimPrefix(req.URL.Path, "/update/")
+		metricType, tail, _ := strings.Cut(tail, "/")
+		metricName, metricValue, _ := strings.Cut(tail, "/")
+
+		switch metricType {
+		case "gauge":
+			if len(metricName) == 0 {
+				http.NotFound(res, req)
+				return
+			}
+			f64, err := strconv.ParseFloat(metricValue, 64)
+			if err != nil {
+				http.Error(res, "incorrect metric value",
+					http.StatusBadRequest)
+				return
+			}
+			repo.SetGauge(metricName, f64)
+		case "counter":
+			if len(metricName) == 0 {
+				http.NotFound(res, req)
+				return
+			}
+			i64, err := strconv.ParseInt(metricValue, 10, 64)
+			if err != nil {
+				http.Error(res, "incorrect metric value",
+					http.StatusBadRequest)
+				return
+			}
+			repo.IncreaseCounter(metricName, i64)
+		case "":
+			http.Error(res, "empty metric type",
+				http.StatusBadRequest)
+			return
+		default:
+			http.Error(res, "unexpected metric type",
+				http.StatusBadRequest)
+			return
+		}
+	}
+}
