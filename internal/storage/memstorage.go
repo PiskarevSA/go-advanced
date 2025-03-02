9ed423d65b5002cef7ba88d84f229884ecf4d354
Semen Piskarev <piskarevsa@mail.ru>
2025-03-02 17:43:02 +0300

    add mutex to memstorage

--- internal/storage/memstorage.go
+++ internal/storage/memstorage.go
@@ -1,6 +1,10 @@
 package storage
 
+import "sync"
+
 type MemStorage struct {
+	mutex sync.Mutex
+
 	gauge   map[string]float64
 	counter map[string]int64
 }
@@ -13,19 +17,27 @@ func NewMemStorage() *MemStorage {
 }
 
 func (m *MemStorage) SetGauge(key string, value float64) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.gauge[key] = value
 }
 
 func (m *MemStorage) Gauge(key string) (value float64, exist bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	value, exist = m.gauge[key]
 	return
 }
 
 func (m *MemStorage) IncreaseCounter(key string, addition int64) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.counter[key] += addition
 }
 
 func (m *MemStorage) Counter(key string) (value int64, exist bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	value, exist = m.counter[key]
 	return
 }
