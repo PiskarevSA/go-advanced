db1892da3435c46c28637bfda4ed39b9f1c7cb27
Semen Piskarev <piskarevsa@mail.ru>
2025-03-03 17:04:20 +0300

    server: add environment vars

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/PiskarevSA/go-advanced
 go 1.22.12
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/stretchr/testify v1.10.0
 )
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -2,12 +2,17 @@ package server
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
+
+	"github.com/caarlos0/env/v6"
 
 	"github.com/PiskarevSA/go-advanced/internal/storage"
 )
 
-var serverAddress = flag.String("a", "localhost:8080", "server address")
+type Config struct {
+	ServerAddress string `env:"ADDRESS"`
+}
 
 type Server struct {
 	storage *storage.MemStorage
@@ -21,13 +26,27 @@ func NewServer() *Server {
 
 // run server successfully or return error to panic in the main()
 func (s *Server) Run() error {
+	var config Config
+	// flags takes less priority according to task description
+	flag.StringVar(&config.ServerAddress, "a", "localhost:8080",
+		"server address, env: ADDRESS")
 	flag.Parse()
 	if flag.NArg() > 0 {
 		flag.Usage()
 		return nil
 	}
+	fmt.Printf("config after flags: %+v\n", config)
+
+	// enviromnent takes higher priority according to task description
+	err := env.Parse(&config)
+	if err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		return nil
+	}
+	fmt.Printf("config after env: %+v\n", config)
 
 	r := MetricsRouter(s.storage)
-	err := http.ListenAndServe(*serverAddress, r)
+	err = http.ListenAndServe(config.ServerAddress, r)
 	return err
 }
