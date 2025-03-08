f2b5b4dbc96a963bda33bf551655d7c53b417e70
Semen Piskarev <piskarevsa@mail.ru>
2025-03-08 14:44:29 +0300

    main: add defer os.Exit(exitCode)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/PiskarevSA/go-advanced/internal/app/agent"
 )
 
 func main() {
+	exitCode := 0
+	defer func() {
+		os.Exit(exitCode)
+	}()
+
 	agent := agent.NewAgent()
 	log.Println("[agent] run")
 	err := agent.Run()
 	if err != nil {
-		// TODO PR #5
-		// Вместо паник и os.Exit'ов всегда стоит использовать log.Fatal, паники
-		// и os.Exit экстренно завершают приложения не позволяя выполнится
-		// defer'ам => не получится graceful shutdown
-		//
-		// поправить надо во всём коде
-		panic(err)
+		log.Println(err)
+		exitCode = 1
 	}
 	log.Println("[agent] shutdown")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/PiskarevSA/go-advanced/internal/app/server"
 )
 
 func main() {
+	exitCode := 0
+	defer func() {
+		os.Exit(exitCode)
+	}()
+
 	server := server.NewServer()
+	log.Println("[server] run")
 	err := server.Run()
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		exitCode = 1
 	}
+	log.Println("[server] shutdown")
 }
